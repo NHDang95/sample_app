13a0a063bb38a13ca6affcedc4015b2dacb411fc
NHDang95 <2d2l95@gmail.com>
2018-04-09 20:23:06 +0700

    Implement basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			#Handle a successful save
+			log_in @user
 			flash[:message] = "Welcome"
 			redirect_to @user
 		else
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
   root "static_pages#home"  # "controller#action"
 
   get '/help', to: 'static_pages#help'
